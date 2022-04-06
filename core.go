e35f2571c31bf95c2896b248aa793e485c7b1b8d
Mellyza Rismawan <mellyza724@gmail.com>
2022-04-06 18:40:51 +0700

    added dana inquiry user info function

--- core.go
+++ core.go
@@ -23,12 +23,14 @@ const (
 	USER_PROFILE_PATH       = "alipayplus/member/query/queryUserProfile.htm"
 	DANA_TIME_LAYOUT        = "2006-01-02T15:04:05-07:00"
 	CURRENCY_IDR            = "IDR"
+	INQUIRY_USER_INFO_PATH  = "v1/customers/user/inquiryUserInfoByAccessToken.htm"
 
 	FUNCTION_CREATE_ORDER       = "dana.acquiring.order.createOrder"
 	FUNCTION_QUERY_ORDER        = "dana.acquiring.order.query"
 	FUNCTION_REFUND             = "dana.acquiring.refund.refund"
 	FUNCTION_APPLY_ACCESS_TOKEN = "dana.oauth.auth.applyToken"
 	FUNCTION_USER_PROFILE       = "dana.member.query.queryUserProfile"
+	FUNCTION_INQUIRY_USER_INFO  = "customers.openapi.user.inquiryUserInfoByAccessToken"
 )
 
 // CoreGateway struct
@@ -155,6 +157,27 @@ func (gateway *CoreGateway) UserProfile(reqBody *UserProfileRequestData, accessT
 	return
 }
 
+func (gateway *CoreGateway) InquiryUserInfo(reqBody *InquiryUserInfoRequest, accessToken string) (res InquiryUserInfoResponse, err error) {
+	var response interface{}
+	response, err = gateway.requestToDanaV1(reqBody, accessToken, FUNCTION_INQUIRY_USER_INFO, INQUIRY_USER_INFO_PATH)
+	if err != nil {
+		return
+	}
+
+	var responseByte []byte
+	responseByte, err = json.Marshal(response)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(responseByte, &res)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func (gateway *CoreGateway) requestToDana(reqBody interface{}, accessToken string, headerFunction string, path string) (res ResponseBody, err error) {
 	now := time.Now()
 
@@ -206,6 +229,64 @@ func (gateway *CoreGateway) requestToDana(reqBody interface{}, accessToken strin
 
 	err = gateway.Call("POST", path, headers, requestReader, &res)
 	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (gateway *CoreGateway) requestToDanaV1(reqBody interface{}, accessToken string, headerFunction string, path string) (res interface{}, err error) {
+	now := time.Now()
+
+	head := RequestHeader{}
+	head.Version = gateway.Client.Version
+	head.Function = headerFunction
+	head.ClientID = gateway.Client.ClientId
+	head.ReqTime = now.Format(DANA_TIME_LAYOUT)
+	head.ClientSecret = gateway.Client.ClientSecret
+
+	if accessToken != "" {
+		head.AccessToken = accessToken
+	}
+
+	var id uuid.UUID
+	id, err = uuid.NewUUID()
+	if err != nil {
+		return
+	}
+
+	head.ReqMsgID = id.String()
+
+	req := Request{
+		Head: head,
+		Body: reqBody,
+	}
+
+	sig, err := generateSignature(req, gateway.Client.PrivateKey)
+	if err != nil {
+		err = fmt.Errorf("failed to generate signature: %v", err)
+		log.Println("generateSignature Failed: ", err)
+		return
+	}
+
+	reqJson, err := json.Marshal(reqBody)
+	if err != nil {
+		return
+	}
+
+	log.Println("Dana request: ", string(reqJson))
+
+	headers := map[string]string{
+		"Content-Type": "application/json",
+		"Client-Id":    gateway.Client.ClientId,
+		"Request-Time": now.Format(DANA_TIME_LAYOUT),
+		"Signature":    sig,
+	}
+
+	bodyReq := bytes.NewBuffer(reqJson)
+	err = gateway.Call("POST", path, headers, bodyReq, &res)
+	if err != nil {
+		log.Println("Failed call dana endpoint: ", err)
 		return
 	}
 
